162a9e7abb9a9151d80ce18b6eac46f6f4c65ff5
Runner-Dad84 <vanhoogie@gmail.com>
2024-04-26 16:11:30 -0400

    Change constructor parametetrs to type and length. Type is ship type.

--- ship.js
+++ ship.js
@@ -1,5 +1,6 @@
 class ship {
-    constructor(length, hits, sunk) {
+    constructor(type, length) {
+        this.type = type;
         this.length = 1;
         this.damage = 0;
         this.sunk = false;
