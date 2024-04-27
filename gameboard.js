c7a6f7584268d7fad5fa33c12dc66f549f64315e
Runner-Dad84 <vanhoogie@gmail.com>
2024-04-27 12:45:12 -0400

    Utility functions colConvert and lookUp.

--- gameboard.js
+++ gameboard.js
@@ -1,4 +1,22 @@
+const ship = require ('./ship.js')
+
+
 //to access square on board us me board.board[r][c]
+
+//utility: converts column letter to number for lookUp
+function colCovert (letter){
+    const alphabet = [...Array(7)].map((_,i) => String.fromCharCode(i +97));
+        const item = (el) => el === letter;
+        const num = alphabet.findIndex(item);
+        return num
+};
+//enter board as variablename.board, returns the value of the board
+function lookUp (Let, Num, board){
+    const c = Num;
+    const r = colCovert(Let);
+    const target = board.find((el) => board[c, r]);
+    return target[c];
+}
 
 class gameboard {
     constructor (row, col) {
@@ -11,14 +29,27 @@ class gameboard {
         for (let i = 0; i < this.row; i++){
             let rows = [];
             for (let j = 0; j < this.col; j++){
-                rows.push(0);
+                let value = 0;
+                rows.push(value);
             }
             gameBoard.push(rows);
         }
         return gameBoard;
     }
-    
+    shipLocation(type, origin, dir) {
+        let newShip = new ship(type);
+        return newShip;
+    }
+
+
+
+
 }
+
+let board1 = new gameboard(7, 7);
+board1.board[0][6] = "X";
+
+console.log(lookUp('a', 6, board1.board));
 
 
 module.exports = gameboard
