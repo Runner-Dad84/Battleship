61fb0cfbfa0e6af9a9d16f5ac1f9916da16f0e8f
Runner-Dad84 <vanhoogie@gmail.com>
2024-04-29 22:00:43 -0400

    Test for updateBoardSqVal method.

--- gameboard.test.js
+++ gameboard.test.js
@@ -1,4 +1,6 @@
-const gameboard = require ('./gameboard.js')
+//const gameboard = require ('./gameboard.js')
+import { gameboard, lookUp } from './gameboard.js'
+
 
 describe('gameboard', ()=> {
     let newboard;
@@ -8,12 +10,43 @@ describe('gameboard', ()=> {
         newboard = new gameboard(3,3);
     });
 
-    test ('check value', () => {
+    test ('check value 1', () => {
         expect(newboard.board[0][0]).toBe(0);
     });
-    test ('check value', () => {
+    test ('check value 2', () => {
         newboard.board[2][3] = 'X';
         expect(newboard.board[2][3]).toBe('X');
     });
 
+})
+
+describe('gameboard 2', ()=> {
+    let testBoard;
+    
+    beforeEach(() => {
+        testBoard = new gameboard(7,7);
+    });
+    test ('look up value Y', () => {
+        testBoard.board[0][0] = 'Y';
+        expect(lookUp('a', 0, testBoard)).toBe('Y');
+    });
+
+    test ('look up value Z', () => {
+        testBoard.board[1][3] = 'Z';
+        expect(lookUp('b', 3, testBoard)).toBe('Z');
+       
+    });
+})
+
+describe('gameboard', ()=> {
+    let board3;
+    
+    beforeEach(() => {
+        board3 = new gameboard(7,7);
+    });
+
+    test ('change value', () => {
+        board3.updateBoardSqVal(2, 4, 'X');
+        expect(board3.board[2][4]).toBe('X');
+    });
 })
