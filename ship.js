cdc14d0d78e4ad67e2d686b486cb58923a2691bd
Runner-Dad84 <vanhoogie@gmail.com>
2024-04-29 21:55:43 -0400

    Add id property to ship class.

--- ship.js
+++ ship.js
@@ -1,28 +1,34 @@
 class ship {
     constructor(type) {
         this.type = type;
+        this.id = 0;
         this.length = 0;
         this.damage = 0;
         this.sunk = false;
 
         if (this.type === 'carrier'){
             this.length = 5;
+            this.id = 'C';
         };
 
         if (this.type === 'battleship'){
             this.length = 4;
+            this.id = 'B';
         };
 
         if (this.type === 'destroyer'){
             this.length = 3;
+            this.id = 'D';
         };
 
         if (this.type === 'submarine'){
             this.length = 3;
+            this.id = 'S';
         };
 
         if (this.type === 'patrol'){
             this.length = 2;
+            this.id = 'P';
         };
     }
     
