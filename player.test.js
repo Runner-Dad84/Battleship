8017ec2516c9b5150df3c6a98819e7c3e25c52ac
Runner-Dad84 <vanhoogie@gmail.com>
2024-05-14 21:00:34 -0400

    Revert "Move test files to test sub directory."
    This reverts commit e1f40432bbffc4854fa6420bb392a7a3790e5d9b.

---
+++ player.test.js
@@ -0,0 +1,25 @@
+//const humanPlayer = require ('./player.js')
+//const compPlayer = require ('./player.js')
+import { humanPlayer, compPlayer } from './player.js'
+
+describe('create players', ()=> {
+    const dave = new compPlayer('Dave', 'easy')
+    const eddie = new humanPlayer('Eddie', 'easy')
+    
+        
+    test ('make computer player', () => {
+        expect(dave.name).toBe('Dave');
+        expect(dave.level).toBe('easy');
+    });
+    test ('make human player', () => {
+        expect(eddie.name).toBe('Eddie');
+        expect(eddie.wins).toBe(0);
+    });
+
+    test ('set board value on computer', () => {
+        dave.comp.board[4][6] = '!';
+        expect(dave.comp.board[4][6]).toBe('!');
+        
+    })
+
+})
