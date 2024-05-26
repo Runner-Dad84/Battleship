aeea76bfccc11c5dd4a8a57a5c5286c0f9694556
Runner-Dad84 <vanhoogie@gmail.com>
2024-05-25 20:45:31 -0400

    add printBoard parameter player and add dataset.

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import { humanPlayer, compPlayer } from './modules/player.js'
 import { printBoard } from './modules/dom.js'
 import './styles/gameboard.style.css';
 
+//create players
 let player1 = new humanPlayer('Andrew');
 let computer = new compPlayer('computer', 'easy');
 
+//set ships
 player1.gb.placeNewShip('carrier', 0, 0, 'south');
 player1.gb.placeNewShip('battleship', 4, 1, 'east');
 player1.gb.placeNewShip('submarine', 1, 1, 'east');
 player1.gb.placeNewShip('destroyer', 6, 0, 'east');
 player1.gb.placeNewShip('patrol', 0, 4, 'south');
-
 
 computer.gb.placeNewShip('carrier', 5, 2, 'north');
 computer.gb.placeNewShip('battleship', 0, 0, 'east');
@@ -21,20 +22,28 @@ computer.gb.placeNewShip('destroyer', 6, 4, 'east');
 computer.gb.placeNewShip('patrol', 4, 6, 'north');
 
 
-printBoard(player1.gb.board, 'container-p1');
-printBoard(computer.gb.board, 'container-p2');
-
 let boardTest = player1.gb.board;
-
+/*
 console.log(boardTest[0][0].value);
 console.log(boardTest[3][4].c);
 console.log(boardTest[3][4].r);
-
+*/
+//attacks
 player1.gb.receiveAttack(0, 1);
 player1.gb.receiveAttack(1, 0);
 player1.gb.receiveAttack(6, 1);
 player1.gb.receiveAttack(1, 1);
 player1.gb.receiveAttack(0, 4);
+computer.gb.receiveAttack(0, 0);
+computer.gb.receiveAttack(6, 4);
+
+//update game board
+printBoard(player1.gb.board, 'container-p1', player1.gb);
+printBoard(computer.gb.board, 'container-p2', computer.gb);
+
+
 console.log(boardTest);
 console.log(player1.gb.ships);
 console.log(player1.gb.ships[0]);
+
+
--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,16 +1,22 @@
 import { gameboard } from './gameboard.js'
 
-export function printBoard (board, container){
+export function printBoard (board, container, player){
     let gb = document.getElementById(container);
     for (let row of board){
         for(let cell of row){
-            let cellEl = document.createElement('div');
-            cellEl.classList.add('cell');
-            cellEl.textContent = cell.value;
-            cellEl.addEventListener('click', ()=> {
-                console.log(cellEl.innerText);
+            let element = document.createElement('div');
+            element.classList.add('cell');
+            element.textContent = cell.value;
+            element.dataset.row = cell.r;
+            element.dataset.col = cell.c;
+            element.addEventListener('click', ()=> {
+                let row = element.dataset.row;
+                let col = element.dataset.col;
+                
+                player.receiveAttack(row, col);
+                //console.log(element);
             })
-            gb.appendChild(cellEl);
+            gb.appendChild(element);
         }
     }
 }
