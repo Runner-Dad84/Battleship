327eaf14a5d088dda5563f5a97388b1cf8605f20
Runner-Dad84 <vanhoogie@gmail.com>
2024-06-04 22:14:23 -0400

    placeNewShip down direction update try-catch statements to throw new error.

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -57,19 +57,21 @@ export class gameboard {
         if (dir === 'Down'){
             //check if ship on board
             try {
-                if ((r-1) + newShip.length > 6) throw "You've gone off course! Redeploy ship."
-            }
-            catch (err) {
-                return console.log(err)
+                if ((r-1) + newShip.length > 6) {throw new Error("You've gone off course! Redeploy ship.")
+                }
+            } catch (error) {
+                console.error('The token is paced off the board:', error.message);
+                return
             }
             //check if spaces are available
             try {
                 for (let i = 0; (row + i) < (row + newShip.length); i++){
-                    if ((this.board[row++][c]).value !== 0) throw 'Near collison! Move that ship.'
+                    if ((this.board[row++][c]).value !== 0) {throw new Error('Near collison! Move that ship.')
+                    }
                 }
-            }
-            catch (err) {
-                return console.log(err);
+            } catch (error) {
+                console.error('Spaces are occupied:', error.message);
+                return 
             }
             //if no errors place ship
             for (let i = 0; (r + i) < (r + newShip.length); i++) {
@@ -79,7 +81,7 @@ export class gameboard {
         if (dir === 'Up'){
             //check if ship on board
             try {
-                if (r - (newShip.length-1) < 0) {throw new Error('Off board! Redeploy ship');
+                if (r - (newShip.length-1) < 0) {throw new Error("You've gone off course! Redeploy ship.");
             }
             } catch (error) {
                 console.error('The token is paced off the board:', error.message);
@@ -88,11 +90,11 @@ export class gameboard {
             //check if spaces are available
             try {
                 for (let i = 0; (row - i) > (row - newShip.length); i++){
-                    if (this.board[row--][c].value !== 0) {throw new Error('Spaces are occupied')
+                    if (this.board[row--][c].value !== 0) {throw new Error('Near collison! Move that ship')
                     }
                 }
             } catch (error) {
-                console.error('Near collison! Move that ship:', error.message);
+                console.error('Spaces are occupied:', error.message);
                 return
             }
             //if no errors place ship
